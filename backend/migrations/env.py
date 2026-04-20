b41904f9a3cc6459944a76fe6cc74b454f2d5cf0
Lowingx <gusta03112008@gmail.com>
2026-04-20 16:40:54 -0300

    feat: estrutura de banco finalizada e tabelas criadas

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -1,25 +1,52 @@
 import os
-from dotenv import load_dotenv
-from logging.config import fileConfig
+import sys
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from alembic import context
-from app.models.base import Base
-from app.models import *
 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'backend')))
 
-load_dotenv()
+from app.models import Base
 
 config = context.config
 
-database_url = os.getenv("DATABASE_URL")
-
-if not database_url:
-    raise ValueError("DATABASE_URL não definida")
-
-config.set_main_option("sqlalchemy.url", database_url)
-
-if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
-
 target_metadata = Base.metadata
+
+sqlalchemy_url = os.getenv("DATABASE_URL")
+
+if not sqlalchemy_url:
+    sqlalchemy_url = f"postgresql+psycopg2://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST', 'db')}:{os.getenv('POSTGRES_PORT', '5432')}/{os.getenv('POSTGRES_DB')}"
+
+config.set_main_option("sqlalchemy.url", sqlalchemy_url)
+
+def run_migrations_offline():
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+def run_migrations_online():
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection, target_metadata=target_metadata
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
